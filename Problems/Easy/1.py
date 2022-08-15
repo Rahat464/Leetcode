dfa3d624a199e47fb76e3899bfbcfab404d6ea4d
Rahat <44652188+TQRL@users.noreply.github.com>
2022-08-15 23:13:19 +0100

    Easy - 1

---
+++ Problems/Easy/1.py
@@ -0,0 +1,14 @@
+nums = [3,2,3]
+target = 6
+
+def twoSum():
+    length = len(nums)
+    for i in range(0, length - 1):
+        result = target - nums[i]
+
+        for j in range(i+1, length):
+            if nums[j] == result:
+                answer = [nums[i], nums[j]]
+                return answer
+
+print(twoSum())
